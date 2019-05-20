2a6ecf2f026a80d3038ebb4bf2782c5495242c5c
Wellington Thiago da Silva <zatara198@gmail.com>
2019-05-19 23:29:59 -0300

    Ajustes no endpoint /categorias/{id}

--- src/main/java/com/wellingtonthiago/cursomc/services/CategoriaService.java
+++ src/main/java/com/wellingtonthiago/cursomc/services/CategoriaService.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Service;
 
 import com.wellingtonthiago.cursomc.domain.Categoria;
 import com.wellingtonthiago.cursomc.repositories.CategoriaRepository;
+import com.wellingtonthiago.cursomc.services.exception.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
@@ -14,6 +15,9 @@ public class CategoriaService {
 	
 	public Categoria buscar(Integer id) {
 		Categoria obj = repo.findOne(id);
+		
+		if(obj ==  null)
+			throw new ObjectNotFoundException("Objeto não encontrado! Id.: "+ id + ", Tipo: "+Categoria.class.getName());
 		return obj;
 	}
 }
