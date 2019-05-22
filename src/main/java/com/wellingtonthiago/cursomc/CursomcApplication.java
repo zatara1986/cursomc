28450f4ae887f26bf52760efeef2ee7b5ac19387
Wellington Thiago da Silva <zatara198@gmail.com>
2019-05-21 22:19:12 -0300

    Pedido, Estado Pagamento e Pagamento

--- src/main/java/com/wellingtonthiago/cursomc/CursomcApplication.java
+++ src/main/java/com/wellingtonthiago/cursomc/CursomcApplication.java
@@ -1,5 +1,6 @@
 package com.wellingtonthiago.cursomc;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,13 +13,20 @@ import com.wellingtonthiago.cursomc.domain.Cidade;
 import com.wellingtonthiago.cursomc.domain.Cliente;
 import com.wellingtonthiago.cursomc.domain.Endereco;
 import com.wellingtonthiago.cursomc.domain.Estado;
+import com.wellingtonthiago.cursomc.domain.Pagamento;
+import com.wellingtonthiago.cursomc.domain.PagamentoComBoleto;
+import com.wellingtonthiago.cursomc.domain.PagamentoComCartao;
+import com.wellingtonthiago.cursomc.domain.Pedido;
 import com.wellingtonthiago.cursomc.domain.Produto;
+import com.wellingtonthiago.cursomc.domain.enums.EstadoPagamento;
 import com.wellingtonthiago.cursomc.domain.enums.TipoCliente;
 import com.wellingtonthiago.cursomc.repositories.CategoriaRepository;
 import com.wellingtonthiago.cursomc.repositories.CidadeRepository;
 import com.wellingtonthiago.cursomc.repositories.ClienteRepository;
 import com.wellingtonthiago.cursomc.repositories.EnderecoRepository;
 import com.wellingtonthiago.cursomc.repositories.EstadoRepository;
+import com.wellingtonthiago.cursomc.repositories.PagamentoRepository;
+import com.wellingtonthiago.cursomc.repositories.PedidoRepository;
 import com.wellingtonthiago.cursomc.repositories.ProdutoRepository;
 
 @SpringBootApplication
@@ -41,6 +49,12 @@ public class CursomcApplication implements CommandLineRunner{
 	
 	@Autowired
 	private EnderecoRepository enderecoRepository;
+	
+	@Autowired
+	private PedidoRepository pedidoRepository;
+	
+	@Autowired
+	private PagamentoRepository pagamentoRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -84,10 +98,26 @@ public class CursomcApplication implements CommandLineRunner{
 		Endereco e1 = new Endereco(null, "Rua Flores", "300", "Apto 300", "Jardim", "38220834", cli1, c1);
 		Endereco e2 = new Endereco(null, "Avenida Matos", "105", "Sala 800", "Centro", "38777012", cli1, c2);
 		
-		cli1.getEnderecos().addAll(Arrays.asList(e1, e2));
+		//cli1.getEnderecos().addAll(Arrays.asList(e1, e2));
 		
 		clienteRepository.save(Arrays.asList(cli1));
 		enderecoRepository.save(Arrays.asList(e1,e2));
+		
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm");
+		
+		Pedido ped1 = new Pedido(null, sdf.parse("30/09/2017 10:32"), cli1, e1);
+		Pedido ped2 = new Pedido(null, sdf.parse("10/10/2017 19:35"), cli1, e2);
+		
+		Pagamento pagto1 = new PagamentoComCartao(null, EstadoPagamento.QUITADO, ped1, 6);
+		ped1.setPagamento(pagto1);
+		
+		Pagamento pagto2 = new PagamentoComBoleto(null, EstadoPagamento.PENDENTE, ped2, sdf.parse("20/10/2017 00:00"), null);
+		ped2.setPagamento(pagto2);
+		
+		cli1.getPedidos().addAll(Arrays.asList(ped1, ped2));
+		
+		pedidoRepository.save(Arrays.asList(ped1, ped2));
+		pagamentoRepository.save(Arrays.asList(pagto1,pagto2));
 	}
 
 }
---
+++ src/main/java/com/wellingtonthiago/cursomc/domain/enums/EstadoPagamento.java
@@ -0,0 +1,35 @@
+package com.wellingtonthiago.cursomc.domain.enums;
+
+public enum EstadoPagamento {
+	PENDENTE(1, "Pendende"), 
+	QUITADO(2, "Quitado"), 
+	CANCELADO(3, "Cancelado");
+	
+	private int cod;
+	private String descricao;
+	
+	private EstadoPagamento(int cod, String descricao) {
+		this.cod = cod;
+		this.descricao = descricao;
+	}
+
+	public int getCod() {
+		return cod;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+	
+	public static EstadoPagamento toEnum(Integer cod) {
+		if(cod == null)
+			return null;
+		
+		for (EstadoPagamento estado : EstadoPagamento.values()) {
+			if(cod.equals(estado.getCod()))
+				return estado;
+		}
+		
+		throw new IllegalArgumentException("Id. inválido: "+ cod);
+	}
+}
