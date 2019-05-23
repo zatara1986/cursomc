ed80868495b845aaaf1634819e538d1ca80c1955
Wellington Thiago da Silva <zatara198@gmail.com>
2019-05-22 23:16:01 -0300

    Deletando CATEGORIA com DELETE

--- src/main/java/com/wellingtonthiago/cursomc/services/CategoriaService.java
+++ src/main/java/com/wellingtonthiago/cursomc/services/CategoriaService.java
@@ -1,10 +1,12 @@
 package com.wellingtonthiago.cursomc.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.wellingtonthiago.cursomc.domain.Categoria;
 import com.wellingtonthiago.cursomc.repositories.CategoriaRepository;
+import com.wellingtonthiago.cursomc.services.exception.DataIntegrityException;
 import com.wellingtonthiago.cursomc.services.exception.ObjectNotFoundException;
 
 @Service
@@ -29,5 +31,14 @@ public class CategoriaService {
 	public Categoria update(Categoria obj) {
 		find(obj.getId());
 		return repo.save(obj);
+	}
+	
+	public void delete(Integer id) {
+		find(id);
+		try {
+			repo.delete(id);
+		}catch(DataIntegrityViolationException e){
+			throw new DataIntegrityException("Não é possível excluir umma categoria que possui produtos!");
+		}
 	}
 }
